842a41acb9e3e6d6e9ed1b16038fa586c667a33d
hyeop29 <87464794+hyeop29@users.noreply.github.com>
2022-09-28 01:24:43 +0900

    Add files via upload

---
+++ 프로그래머스/Lv. 1/약수의 합.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = []
+    for i in range(1,n+1) :
+        if (n % i) == 0 :
+            answer.append(i)
+    answer = sum(answer)
+    return answer
+
+print(solution(12))
+print(solution(5))
